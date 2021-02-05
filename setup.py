80f98014bb65449f6e1955fed2d9dabb97b9648f
andreasxp <andres.zhukov@gmail.com>
2021-02-05 16:39:34 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Setup script for symba-gui package."""
+from setuptools import setup, find_packages
+
+install_requires = [
+    "pyside6"
+]
+
+extras_require = {
+    "freeze":  [
+        "pyinstaller",
+    ]
+}
+
+entry_points = {
+    "gui_scripts": ["symba-gui = symba_gui.__main__:main"],
+    "console_scripts": ["symba-gui-d = symba_gui.__main__:main"]
+}
+
+setup(
+    name="symba-gui",
+    version="0.1",
+    description="Symba market simulator",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    entry_points=entry_points,
+    packages=find_packages(),
+    #package_data=package_data,
+)
---
+++ symba_gui/__main__.py
@@ -0,0 +1,70 @@
+import sys
+from argparse import ArgumentParser
+from subprocess import Popen
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenu, QWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        menu_bar = self.menuBar()
+
+        menu_file = menu_bar.addMenu("File")
+        
+        action_new = menu_file.addAction("New Simulation")
+        action_new.triggered.connect(self.actionNew)
+
+        menu_file.addSeparator()
+        menu_file.addAction("Open...")
+        menu_file.addSeparator()
+        menu_file.addAction("Save")
+        menu_file.addAction("Save As...")
+        menu_file.addSeparator()
+        action_exit = menu_file.addAction("Exit")
+        action_exit.triggered.connect(self.actionExit)
+
+    def actionNew(self):
+        """Start a new experiment. Opens a new window.
+        
+        This process launches a new simulation window by creating a detached process. The way the process is created
+        depends on sys.argv[0] (what is the origin of this process).
+        """
+        pos_x = self.pos().x() + 40
+        pos_y = self.pos().y() + 40
+        cli_args = ["--window-pos", f"{pos_x},{pos_y}"]
+
+        if sys.argv[0].endswith(".py"):
+            # Application launched with python
+            Popen([sys.executable, sys.argv[0]] + cli_args)
+        else:
+            # Assume the application was launched using an executable
+            Popen([sys.argv[0]] + cli_args)
+        
+    def actionExit(self):
+        self.close()
+
+
+def main():
+    print(sys.argv[0])
+    parser = ArgumentParser()
+    parser.add_argument("--window-pos", help="new window position on the screen")
+
+    args = parser.parse_args()
+    if args.window_pos is not None:
+        args.window_pos = [int(x) for x in args.window_pos.split(",")]
+
+    app = QApplication(sys.argv)
+
+    main_window = MainWindow()
+    if args.window_pos is not None:
+        main_window.move(*args.window_pos)
+
+    main_window.show()
+
+    return app.exec_()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
